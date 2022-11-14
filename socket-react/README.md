7da10c7838298827b3a00476094ff6091baccd2d
emanuelefavero <emafaux89@gmail.com>
2022-11-14 08:14:30 +0100

    add socket-react app

---
+++ socket-react/README.md
@@ -0,0 +1,17 @@
+# Socket.IO with React
+
+This is a simple example of how to use Socket.IO with React.
+
+## Installation
+
+```bash
+npm install
+```
+
+## Usage
+
+```bash
+npm start
+```
+
+- Go to [http://localhost:3000](http://localhost:3000)
---
+++ socket-react/server/server.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const express = require('express')
+const app = express()
+const http = require('http')
+const server = http.createServer(app)
+const { Server } = require('socket.io')
+const io = new Server(server)
+
+io.on('connection', (socket) => {
+  console.log('a user connected')
+  socket.emit('user connected', 'A user connected')
+
+  socket.on('ping', () => {
+    socket.emit('pong')
+  })
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected')
+    socket.emit('user disconnected', 'A user disconnected')
+  })
+})
+
+app.use(express.static(path.join(__dirname, '../build')))
+app.use(express.static('public'))
+
+app.use((rea, res, next) => {
+  res.sendFile(path.join(__dirname, '../build/index.html'))
+})
+
+server.listen(3000, () => {
+  console.log('Server is running on port 3000')
+})
