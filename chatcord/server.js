78efe0cb73ab1a7cdb4cb9cec476c7b0ccf6825e
emanuelefavero <emafaux89@gmail.com>
2022-11-14 15:28:51 +0100

    chatcord - add - messages.js

--- chatcord/server.js
+++ chatcord/server.js
@@ -3,6 +3,8 @@ const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
 
+const formatMessage = require('./utils/messages')
+
 const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
@@ -10,23 +12,28 @@ const io = socketio(server)
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')))
 
+const botName = 'ChatCord Bot'
+
 // Run when client connects
 io.on('connection', socket => {
   // Welcome current user
-  socket.emit('message', 'Welcome to ChatCord!')
+  socket.emit('message', formatMessage(botName, 'Welcome to ChatCord!'))
 
   // Broadcast when a user connects (By using the broadcast method, we can send a message to everyone except for the user that is connecting)
-  socket.broadcast.emit('message', 'A user has joined the chat')
+  socket.broadcast.emit(
+    'message',
+    formatMessage(botName, 'A user has joined the chat')
+  )
 
   // Runs when client disconnects
   socket.on('disconnect', () => {
-    io.emit('message', 'A user has left the chat')
+    io.emit('message', formatMessage(botName, 'A user has left the chat'))
   })
 
   // Listen for chatMessage
   socket.on('chatMessage', msg => {
     // Emit the msg from the client input to everybody, using io.emit
-    io.emit('message', msg)
+    io.emit('message', formatMessage('USER', msg))
   })
 })
 
