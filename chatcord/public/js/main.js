d1e77fab5a15640e673997fc11766abcf2c9c0cb
emanuelefavero <emafaux89@gmail.com>
2022-11-15 01:50:08 +0100

    chatcord - add - custom username color for current user

--- chatcord/public/js/main.js
+++ chatcord/public/js/main.js
@@ -54,7 +54,9 @@ chatForm.addEventListener('submit', (e) => {
 function outputMessage(message) {
   const div = document.createElement('div')
   div.classList.add('message')
-  div.innerHTML = `<p class="meta">${message.username} <span>${message.time}</span></p>
+  div.innerHTML = `<p class="meta ${
+    message.username === username && 'current-user'
+  }">${message.username} <span>${message.time}</span></p>
   <p class="text">
     ${message.text}
   </p>`
